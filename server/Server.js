6f42497d68cc34722f29875231da07b76d70182a
JessicaBane <jessicambane@gmail.com>
2023-07-31 11:02:05 +0100

    server working

--- server/Server.js
+++ server/Server.js
@@ -3,16 +3,16 @@ const cors = require("cors")
 require("dotenv").config()
 const PORT = process.env.PORT||8084
 const app = express()
-app.use(cors)
-app.listen(PORT, ()=> console.log(`app is listening on PORT ${PORT}`))
-const data = require("./data.json")
+app.use(cors())
 
 const bags = require("./Lib/Bags")
 const clothes = require("./Lib/Clothes")
 
-app.get("/", (request, response)=>{
+app.get("/", function(request, response){
     response.status(200).json("This is the main root")
 })
 
 app.get("/bags", bags)
 app.get("/clothes", clothes)
+
+app.listen(PORT, ()=> console.log(`app is listening on PORT ${PORT}`))
